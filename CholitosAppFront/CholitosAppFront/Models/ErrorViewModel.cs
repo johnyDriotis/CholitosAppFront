37b59049bf16c19702543787c03572dd00541a72
johnyDriotis <johny.driotis1996@gmail.com>
2024-12-07 11:30:25 -0600

    Se agregan archivos inciales del proyecto

---
+++ CholitosAppFront/CholitosAppFront/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CholitosAppFront.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
